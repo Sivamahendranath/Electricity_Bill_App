e58009e8eda2d585c2d8b232f201f6f4264ce4f0
Ragimanu Sivamahendranath <157783369+Sivamahendranath@users.noreply.github.com>
2025-04-05 17:58:00 +0530

    Update bill_calculator.py

--- bill_calculator.py
+++ bill_calculator.py
@@ -8,6 +8,7 @@ class BillCalculator:
         self.INDUSTRIAL_RATE_PEAK = 8.00
         self.INDUSTRIAL_RATE_NORMAL = 6.00
         self.SERVICE_CHARGE_PERCENTAGE = 0.05
+        self.LATE_FEE_PERCENTAGE = 0.02  # 2% late fee
         
     def calculate_domestic_bill(self, units):
         if units <= 0:
@@ -37,7 +38,7 @@ class BillCalculator:
             
         return peak_hour_charge + normal_hour_charge
     
-    def calculate_bill(self, customer_type, current_reading, previous_reading, peak_hour_units=0):
+    def calculate_bill(self, customer_type, current_reading, previous_reading, bill_date, peak_hour_units=0):
         if current_reading < previous_reading:
             raise ValueError("Current reading cannot be less than previous reading")
             
@@ -54,11 +55,22 @@ class BillCalculator:
             
         service_charge = net_bill * self.SERVICE_CHARGE_PERCENTAGE
         total_bill = net_bill + service_charge
+        
+        # Calculate due date (21 days from bill date)
+        import datetime
+        bill_date_obj = datetime.datetime.strptime(bill_date, "%Y-%m-%d")
+        due_date = (bill_date_obj + datetime.timedelta(days=21)).strftime("%Y-%m-%d")
+        
+        # Calculate late payment charges (if bill is paid after due date)
+        late_fee = round(total_bill * self.LATE_FEE_PERCENTAGE, 2)
             
         return {
             "units_consumed": units,
+            "bill_date": bill_date,
+            "due_date": due_date,
             "net_bill": round(net_bill, 2),
             "service_charge": round(service_charge, 2),
-            "total_bill": round(total_bill, 2)
+            "total_bill": round(total_bill, 2),
+            "late_fee": late_fee,
+            "amount_after_due_date": round(total_bill + late_fee, 2)
         }
-      
